fc88b822da90ab3e4802abd7fbdfa9e77009dd5e
Kaya-Dec24 <saga1998from@icloud.com>
2019-12-17 13:26:34 +0900

    clear

--- archive/stmp5.c
+++ archive/stmp5.c
@@ -1,76 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <netinet/in.h>
-#include <fcntl.h>
+#include <string.h>
+#include <netdb.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <arpa/inet.h>
 
+#define ERR -1
 #define MAX 64
 #define SERVER_IP "127.0.0.1" //local loopback
 #define SMTP 1900
-#define POP 1901
-
-void func(int sockfd) 
-{ 
-    char buff[MAX]; 
-    int n; 
-    // infinite loop for chat 
-    for (;;) { 
-        bzero(buff, MAX); 
-  
-        // read the message from client and copy it in buffer 
-        recv(sockfd, buff, sizeof(buff)); 
-        // print buffer which contains the client contents 
-        printf("From client: %s\t To client : ", buff); 
-        bzero(buff, MAX); 
-        n = 0; 
-        // copy server message in the buffer 
-        while ((buff[n++] = getchar()) != '\n') 
-            ; 
-  
-        // and send that buffer to client 
-        write(sockfd, buff, sizeof(buff)); 
-  
-        // if msg contains "Exit" then server exit and chat ended. 
-        if (strncmp("exit", buff, 4) == 0) { 
-            printf("Server Exit...\n"); 
-            break; 
-        } 
-    } 
-} 
+#define POP 110
 
 int main(){
-	int sd;
-	int acc_sd;
-	struct sockaddr_in addr;
-	struct sockaddr_in from_addr;
-	socklen_t sin_size = sizeof(struct sockaddr_in);
-	char buff[MAX],msg[MAX],from[MAX],txt[]="rec.txt";;
-	FILE *fp;
+    int sock, cli, k;
+    unsigned int len;
+    int sent;
+    //char mesg[] = "HELO";
+    struct sockaddr_in server, client;
+    char buff[MAX], msg[MAX], to[MAX],txt[]="rec.txt", file_dir[MAX];
+    FILE *fp;
 
-	system("clear");
-	// IPv4 TCP のソケットを作成
-	if((sd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("socket");
-		return -1;
-	}
+	//ソケットの作成
+    if(sock = socket(AF_INET, SOCK_STREAM, 0) == ERR)
+    {
+        perror("socket: ");
+        exit(-1);
+    }
 
-    // 待ち受けるIPとポート番号を設定
-	addr.sin_family = AF_INET;		// IPv4 インターネットプロトコル
-	addr.sin_port = htons(SMTP);		// ポート番号
-	addr.sin_addr.s_addr = INADDR_ANY;	// IPアドレス
-						// INADDR_ANY : 全てのローカルインターフェイスにバインドされうる
-	// バインドする
-	if(bind(sd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-		perror("bind");
-		return -1;
-	}
-	// パケット受信待ち状態とする
-	// 待ちうけキューを10としている
-	if(listen(sd, 10) < 0) {
-		perror("listen");
-		return -1;
-	}
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(SMTP);
+    server.sin_addr.s_addr = INADDR_ANY;
+	//bzero(&server.sin_zero, 10);
+
+	//bind
+len = sizeof(struct sockaddr_in);
+if((bind(sock, (struct sockaddr *)&server, len)) == ERR)
+    {
+        perror("bind");
+        exit(-1);
+    }
+
+//listen
+if((listen(sock, 10)) == ERR)
+    {
+        perror("listen");
+        exit(-1);
+    }
+
+while(1)
+    {
+    memset(buff, 0, sizeof(buff));
+    memset(msg, 0, sizeof(msg));
+    memset(to, 0, sizeof(to));
+    printf("waiting for connection¥n");
+    //accept
+    if((cli = accept(sock, (struct sockaddr *)&client, &len)) == ERR)
+    {
+        perror("accept");
+        exit(-1);
+    }
+
+    recv(cli, to, sizeof(msg), 0);
+    printf("connection request to %s¥n",to);
+
+    for(k=0;;k++){
+    sprintf(file_dir,"%s/mail%d.txt", to, k+1);
+    if ((fp = fopen(file_dir, "r")) != NULL)
+	 break;
+    }
+    fp = fopen(file_dir, "w");
+    while(strcmp(msg,".") != 0){
+    recv(cli, msg, sizeof(msg),0);
+        if(strcmp(msg,".") == 0){
+		strcpy(buff, "1");
+    }
+    else{
+		strcpy(buff, "0");
+    }
+	fprintf(fp, "%s\n", msg);
+	send(cli, buff, sizeof(buff), 0);
+    }
+    fclose(fp);
+    close(cli);
+    }
+close(sock);
+return 0;
+}  
